d54a3a7878e622c08276aeabddd18380efa9c027
Roman Teplov <teplov@gmail.com>
2014-04-17 19:35:32 +0400

    configured

--- config.js
+++ config.js
@@ -7,48 +7,37 @@ var path = require('path'),
 
 config = {
     // ### Development **(default)**
-    // development: {
-    //     // The url to use when providing links to the site, E.g. in RSS and email.
-    //     url: 'http://localhost',
-
-    //     // Example mail config
-    //     // Visit http://docs.ghost.org/mail for instructions
-    //     // ```
-    //     //  mail: {
-    //     //      transport: 'SMTP',
-    //     //      options: {
-    //     //          service: 'Mailgun',
-    //     //          auth: {
-    //     //              user: '', // mailgun username
-    //     //              pass: ''  // mailgun password
-    //     //          }
-    //     //      }
-    //     //  },
-    //     // ```
-
-    //     database: {
-    //         client: 'sqlite3',
-    //         connection: {
-    //             filename: path.join(__dirname, '/content/data/ghost-dev.db')
-    //         },
-    //         debug: false
-    //     },
-    //     server: {
-    //         // Host to be passed to node's `net.Server#listen()`
-    //         host: '127.0.0.1',
-    //         // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-    //         port: '2368'
-    //     },
-    //     paths: {
-    //         contentPath: path.join(__dirname, '/content/')
-    //     }
-    // },
+    development: {
+       url: 'http://polar-everglades-2700.herokuapp.com',
+        //url: 'http://polar-everglades-2700.herokuapp.com',
+        mail: {},
+       // fileStorage: false,
+        database: {
+            client: 'postgres',
+            connection: {
+                host: 'ec2-54-225-101-124.compute-1.amazonaws.com',
+                user: 'tglaqgnnaeupsq',
+                password: 'hONaJuUXFhktNpwBFC6he6LWXF',
+                database: 'db233od9ib5nbk',
+                port: '5432',
+                ssl: true
+            },
+            debug: false
+        },
+        server: {
+            // Host to be passed to node's `net.Server#listen()`
+            host: '0.0.0.0',
+            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+            port: process.env.PORT
+        }
+    },
 
     // ### Production
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://polar-everglades-2700.herokuapp.com',
+        //url: 'http://localhost',
+        //url: 'http://polar-everglades-2700.herokuapp.com',
         mail: {},
        // fileStorage: false,
         database: {
