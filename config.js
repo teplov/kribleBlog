1f5569dc17f84de30e1bcac611e8a166fc295893
Roman Teplov <teplov@gmail.com>
2014-04-17 18:18:31 +0400

    configured

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-          url: 'http://my-ghost-blog.com',
+        url: 'http://my-ghost-blog.com',
 
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
@@ -48,7 +48,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-          url: 'http://my-ghost-blog.com',
+        url: process.env.MY_URL,
         mail: {},
         fileStorage: false,
         database: {
