1bc065ec6f3e9327607f0a441e0066c77b4b54fe
Roman Teplov <teplov@gmail.com>
2014-04-17 19:30:20 +0400

    configured

--- config.js
+++ config.js
@@ -7,42 +7,42 @@ var path = require('path'),
 
 config = {
     // ### Development **(default)**
-    development: {
-        // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'http://localhost',
+    // development: {
+    //     // The url to use when providing links to the site, E.g. in RSS and email.
+    //     url: 'http://localhost',
 
-        // Example mail config
-        // Visit http://docs.ghost.org/mail for instructions
-        // ```
-        //  mail: {
-        //      transport: 'SMTP',
-        //      options: {
-        //          service: 'Mailgun',
-        //          auth: {
-        //              user: '', // mailgun username
-        //              pass: ''  // mailgun password
-        //          }
-        //      }
-        //  },
-        // ```
+    //     // Example mail config
+    //     // Visit http://docs.ghost.org/mail for instructions
+    //     // ```
+    //     //  mail: {
+    //     //      transport: 'SMTP',
+    //     //      options: {
+    //     //          service: 'Mailgun',
+    //     //          auth: {
+    //     //              user: '', // mailgun username
+    //     //              pass: ''  // mailgun password
+    //     //          }
+    //     //      }
+    //     //  },
+    //     // ```
 
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-dev.db')
-            },
-            debug: false
-        },
-        server: {
-            // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
-            // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
-        },
-        paths: {
-            contentPath: path.join(__dirname, '/content/')
-        }
-    },
+    //     database: {
+    //         client: 'sqlite3',
+    //         connection: {
+    //             filename: path.join(__dirname, '/content/data/ghost-dev.db')
+    //         },
+    //         debug: false
+    //     },
+    //     server: {
+    //         // Host to be passed to node's `net.Server#listen()`
+    //         host: '127.0.0.1',
+    //         // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
+    //         port: '2368'
+    //     },
+    //     paths: {
+    //         contentPath: path.join(__dirname, '/content/')
+    //     }
+    // },
 
     // ### Production
     // When running Ghost in the wild, use the production environment
@@ -58,9 +58,10 @@ config = {
                 user: 'tglaqgnnaeupsq',
                 password: 'hONaJuUXFhktNpwBFC6he6LWXF',
                 database: 'db233od9ib5nbk',
-                port: '5432'
+                port: '5432',
+                ssl: true
             },
-            debug: true
+            debug: false
         },
         server: {
             // Host to be passed to node's `net.Server#listen()`
